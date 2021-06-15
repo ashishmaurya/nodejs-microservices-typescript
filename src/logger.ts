825764dfcd120ea61d62f64d3b4b5b1d77849d1b
Ashish Maurya <ashishmaurya@outlook.com>
2021-06-15 10:36:00 +0530

    Fixed default logger

--- src/logger.ts
+++ src/logger.ts
@@ -5,15 +5,16 @@ const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   defaultMeta: {
-    service: 'sample-service', lang: 'nodejs', version: '1.0.1', ip: '',
+    service: 'sample-service', lang: 'nodejs', version: '1.0.1',
   },
   transports: [
     //
     // - Write all logs with level `error` and below to `error.log`
     // - Write all logs with level `info` and below to `combined.log`
     //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    // new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    // new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.Console()
   ],
 });
 
