b45fbe1e2cc3614512c29ca1ec7354fd54bc893b
Ashish Maurya <ashishmaurya@outlook.com>
2021-06-15 10:37:03 +0530

    Pull port from env rather then hardcoded value

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const server = app.listen(9091, () => {
+const server = app.listen(process.env.PORT || 8080, () => {
   const address = server.address();
   if (typeof address === 'object') {
     console.log('Server Started on port', address?.port);
